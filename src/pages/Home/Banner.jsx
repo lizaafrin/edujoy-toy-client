58a0322b2bfb1e77a98fc934715d1a1ea6b8b377
lizaafrin <lizaafrin446@gmail.com>
2023-05-18 20:49:29 +0600

    Header and footer added

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import bannerBg from '../../assets/eduBanner.jpg';
+import bannerImage from '../../assets/geography.jpg';
+import { FaLongArrowAltRight } from 'react-icons/fa';
+
+const Banner = () => {
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${bannerBg}), linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,4))`,
+                backgroundBlendMode: "overlay",
+            }}
+            className="bg-cover bg-center text-white px-4 h-[550px] flex items-center justify-center"
+        >
+            <div className="px-5 lg:px-0 lg:w-2/5 w-1/2 text-left">
+                <h2 className="text-white text-4xl lg:text-6xl leading-tight tracking-tighter font-bold">
+                    <span className="text-fuchsia-700">EDUJOY</span>TOY
+                </h2>
+                <p className="text-white py-7 text-xl">
+                Building Blocks of Knowledge: Unleashing Potential with Our Educational Kits!
+                </p>
+                <button className="bg-fuchsia-700 text-white px-6 py-2 rounded-md hover:bg-fuchsia-800 text-xl">
+                    Start Shopping{" "}
+                    <FaLongArrowAltRight className="inline-block"></FaLongArrowAltRight>{" "}
+                </button>
+            </div>
+            <div className='w-1/2'>
+                <img className='rounded-3xl' src={bannerImage} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AllToys from '../AllToys/AllToys';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <AllToys></AllToys>
         </div>
     );
